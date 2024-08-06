4b462a61586455e69a6797c236f910c07bd9ba80
Ebendttl <ebendttl@gmail.com>
2024-08-06 04:02:12 +0100

    Task 1: Fix 1

--- 0x01-python_async_function/0-main.py
+++ 0x01-python_async_function/0-main.py
@@ -8,8 +8,3 @@ print(asyncio.run(wait_random()))
 print(asyncio.run(wait_random(5)))
 print(asyncio.run(wait_random(15)))
 
-bob@dylan:~$ ./0-main.py
-9.034261504534394
-1.6216525464615306
-10.634589756751769
-
